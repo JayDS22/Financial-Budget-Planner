a1219da22a96e0d9f95ce59656baea96ae755db6
Harshit Soora <soora.iitkgp@gmail.com>
2026-02-07 15:43:47 -0500

    env

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
-
+if (!process.env.RAILWAY_ENVIRONMENT) {
+  require('dotenv').config();
+}
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
